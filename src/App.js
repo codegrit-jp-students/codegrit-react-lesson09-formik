21614cfd074bb68219d015c89adc0ed35c5154fc
Atsuhiro Teshima <hiro@lingualbox.com>
2019-05-16 18:23:37 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
+/** @jsx jsx */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { jsx } from '@emotion/core'
+import { Signup } from './Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" css={{ margin: 40 }}>
+      <Signup />
     </div>
   );
 }
